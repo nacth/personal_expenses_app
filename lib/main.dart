614073e018e7c7c1343368c89675401c54bd9d2c
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-08-25 21:40:49 +0700

    build a custom list item

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,21 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
             children: transactions.map((tx) {
-              return Card(child: Text(tx.title),);
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(tx.amount.toString()),
+                    ),
+                    Column(
+                      children: [
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
             }).toList(),
           )
         ],
